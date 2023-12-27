56558f5222f018eadfd2c6388bca6c66496b0c97
muniryasir <muniryasir2009@gmail.com>
2023-12-28 01:43:06 +0500

    task 4 updates

--- src/pages/api/ocr_code_interperation.js
+++ src/pages/api/ocr_code_interperation.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import {useRouter} from "next/router";
 
-const ai_key = process.env.AI_KEY_PB
+const ai_key = process.env.AI_KEY
 const openai = new OpenAI({ apiKey: ai_key });
 
 async function talk_to_AI(query) {
@@ -34,19 +34,32 @@ export default async function  handler(req, res) {
   } else if(req.method == 'GET') {
 
     let code = `
-      
-    Take two arguments from user
-    Take the sum of two arguments
-    Return the sum
+    
+    function validate_user()
+	const pw = "pass"
+	pwguess = ""
+	attempts = 1
+	do
+		print("Attempt number " + attempts)
+		pwguess = input("Password: ")
+		attempts += 1
+	until pwguess == pw OR attempts > 3
 
+	return pwguess == pw
+endfunction
+
+if validate_user() then
+	print("yay")
+else
+	print("no")
+endif
 
     `
     
     
     const question = req.query.question;
-    // let ques = `consider the following code ${code} GCSE OCR ERL Exam Reference Language for Algorithims, is it syntically correct and what is its purpose also please provide eqvalent python code within code tags`
-    let ques = `consider the following pseudocode ${code} provide equvalent python code`
-    // let ques = 'what year it is?'
+    // let ques = `consider the following code ${code} GCSE OCR ERL Exam Reference Language for Algorithims, is it syntically correct and evaulate it qualitativly, also provide eqvalent python code. Return answer as a JSON object with keys code, feedback and python`
+    let ques = `consider the following code ${code} written in GCSE OCR ERL Exam Reference Language for Algorithims, is it correct, if so provide equvlent python code `
     let responseAI = await talk_to_AI(ques);
     res.status(200).json({AI_Answer: responseAI.answer.message.content});
     // res.status(200).json();
